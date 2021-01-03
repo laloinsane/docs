3b9361f41dca7414cf20a81b95a549d810d20468
laloinsane <calisto.michel@gmail.com>
2021-01-03 12:12:28 -0300

    model test

--- example/test.php
+++ example/test.php
@@ -1,5 +1,19 @@
 <?php
 include __DIR__ . "/../vendor/autoload.php";
 
+use laloinsane\docs\User;
+// use laloinsane\docs\Models\User;
+
 $hello = new \laloinsane\docs\Docs();
 echo $hello->sayHello();
+
+$user = new User();
+// $user->name = "lalo";
+// $user->project = "ruv";
+
+// echo $user;
+
+// echo User::create([
+//     'name' => "lalo",
+//     'project' => "ruv",
+// ]);
